b205277985e4a8639ffd5548c15519ab82fd904e
Raul Pereira <raul.pereira@gmail.com>
2019-10-16 00:58:50 -0300

    Initial course

---
+++ infraestrutura/conexao.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql')
+
+const conexao = mysql.createConnection({
+  host: '192.168.99.100',
+  port: 3306,
+  database: 'agenda-petshop',
+  user: 'root',
+  password: 'sa'
+})
+
+module.exports = conexao
